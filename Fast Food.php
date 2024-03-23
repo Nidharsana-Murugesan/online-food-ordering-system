094e8b67c591dca7be149f54ac6e9a8164b9d301
Nidharsana-Murugesan <159101081+Nidharsana-Murugesan@users.noreply.github.com>
2024-03-23 10:28:28 +0530

    Add files via upload

---
+++ Fast Food.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<?php include "header.php" ?>
+</head>
+
+
+<body>
+<?php include "top.php"  ?>
+<div>
+<br/>
+<br/>
+<h2 align = center>Fast Food</h2>
+<?php
+include "dbconfigure.php";
+
+
+$query = "select * from recipebycategory where rtype='fast food'";
+$rs = my_select($query);
+$n = mysql_num_rows($rs);
+echo "<table align = center border = 1 width = 100%>";
+echo "<tr>";
+echo "<td>Recipe Name</td>";
+echo "<td>Type</td>";
+echo "<td>Recipe Image</td>";
+echo "<td>Recipe Ingredient</td>";
+echo "<td>Recipe Description</td>";
+echo "</tr>";
+while($column=mysql_fetch_array($rs))
+{
+
+echo "<tr>";
+
+echo "<td>$column[1]</td>";
+echo "<td>$column[2]</td>";
+echo "<td><img src = '$column[3]' width = 100 height = 100></td>";
+echo "<td>$column[4]</td>";
+echo "<td>$column[5]</td>";
+
+echo "</tr>";
+}
+echo "</table>";
+?>
+</div>
+<!-- Bottom section -->
+<?php include "bottom.php" ?>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,28 @@
+<?php ob_start();?>
+<html>
+<head>
+<?php include "header.php"; ?>
+</head>
+
+
+<body>
+<?php include "top.php";  ?>
+<div>
+<table>
+<tr>
+<td width = 300><b>Existing User : <a href =signin.php>Sign IN</a></b>
+					 <br/>
+					 <br/>
+					 <b>New User : <a href = signup.php>SIGN UP</a></b>
+					 </td></tr></table>
+</div>
+
+
+
+
+
+
+<!-- Bottom section -->
+<?php include "bottom.php"; ?>
+</body>
+</html>
